c6d14afb5c8b5cfc5bee8c6042b080d55701c991
Eliana Garcia <elianagarcia862@gmail.com>
2023-01-25 21:43:16 -0300

    angular app

--- angular-hola-mundo/src/app/app.module.ts
+++ angular-hola-mundo/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SaludadorComponent } from './saludador/saludador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SaludadorComponent
   ],
   imports: [
     BrowserModule
